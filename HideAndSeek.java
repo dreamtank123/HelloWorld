2e978410241161f2bfdb7154b7ac393a57cfac34
Mr.cat <y290639035@gmail.com>
2022-09-09 17:27:53 +0900

    9月9日

---
+++ HideAndSeek.java
@@ -0,0 +1,10 @@
+public class HideAndSeek {
+	public static void main(String[] args) {
+		System.out.println("HideAndSeek");
+		System.out.println("Let paly hid and seek");
+		System.out.print("Three...");
+		System.out.print("Two...");
+		System.out.println("one...");
+		System.out.println("Ready or not,here I come!");
+	}
+}
---
+++ Tree.java
@@ -0,0 +1,13 @@
+public class Tree{
+  public static void main(String[]args) {
+    System.out.println("Hey there, I’m Ariel and I’m learning to code in Java!");
+    System.out.println("I’m going to plant a tree today!");
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println(" ***** ");
+    System.out.println("   *  ");
+    System.out.println("   *  ");
+    System.out.println("#######"); 
+ 
+  }
+}
